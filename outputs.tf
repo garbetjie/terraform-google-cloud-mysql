39673a673498b72acabce232f83fa8af4815a07b
Geoff Garbers <geoff@garbers.co.za>
2020-09-16 12:14:19 +0200

    Add self_link outputs, as well as users.

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output master_address {
   value = google_sql_database_instance.master.public_ip_address
 }
 
+output master_self_link {
+  value = google_sql_database_instance.master.self_link
+}
+
 output master_private_address {
   value = google_sql_database_instance.master.private_ip_address
 }
@@ -30,6 +34,24 @@ output readonly_replica_name {
   value = length(google_sql_database_instance.read_replica) > 0 ? google_sql_database_instance.read_replica[0].name : null
 }
 
+output readonly_replica_self_link {
+  value = length(google_sql_database_instance.read_replica) > 0 ? google_sql_database_instance.read_replica[0].self_link : null
+}
+
 output readonly_replica_connection_name {
   value = length(google_sql_database_instance.read_replica) > 0 ? google_sql_database_instance.read_replica[0].connection_name : null
+}
+
+output users {
+  // We need to do it like this because as per https://www.terraform.io/docs/providers/mysql/r/user.html, the password
+  // is salted before being stored in the state.
+  value = [
+    for user in mysql_user.users: {
+      host = user.host
+      username = user.user
+      password = (local.users[format("%s@%s", user.user, user.host)].password == ""
+        ? random_password.user_passwords[format("%s@%s", user.user, user.host)].result
+        : local.users[format("%s@%s", user.user, user.host)].password)
+    }
+  ]
 }
