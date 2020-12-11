2241bb976f78d560deefd31b11f8f61f69d9f8d2
Geoff Garbers <geoff@garbers.co.za>
2020-12-11 09:05:41 +0200

    Ignore settings.0.replication_type attribute, as it is legacy and causing issues as per https://github.com/hashicorp/terraform-provider-google/issues/7990

--- main.tf
+++ main.tf
@@ -68,6 +68,6 @@ resource google_sql_database_instance master {
   }
 
   lifecycle {
-    ignore_changes = [settings[0].disk_size]
+    ignore_changes = [settings[0].disk_size, settings[0].replication_type]
   }
 }
